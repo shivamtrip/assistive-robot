d68d033c3ff9158e99ed950d483c36669f089224
shivamtrip <shivamtrip99@gmail.com>
2023-11-03 12:05:32 -0400

    mission_planner initial

---
+++ src/planning/mission_planner/scripts/mission_planner.py
@@ -0,0 +1,59 @@
+
+
+
+
+
+class States(Enum):
+    
+    IDLE = 1
+
+
+
+class Mission_Planner():
+    
+    def __init__(self):
+        
+        self.task_queue = 
+        
+        
+        listening_service()         # callback: task_allocator
+        
+        
+        
+    def task_allocator():
+        """
+        Task Allocator adds information regarding the newly added task to the task queue    
+        """
+    
+    def health_manager():
+        """
+        Health Manager updates battery status (and other health parameters, if applicable) 
+        """
+    
+            
+    def update_next_task():
+        
+        next_task = task_queue
+        
+        self.current_task_type = 
+        self.current_task_object = 
+        self.current_task_room = 
+        self.current_goal_x = 
+        self.current_goal_y = 
+        
+                    
+            
+    def main():
+
+        if not self.battery_health_ok or not self.task_queue or system_state != STATES.IDLE:
+            print("Cannot perform next task")
+    
+        if self.task_queue:
+            self.update_next_task()         # Updates all relevant parameters to perform next task
+
+
+                
+        
+
+    
+    
