7f8dd2b4a03de2d42a6d33ff600ef6c277b9645c
praveenVnktsh <pveenvenkatesh@gmail.com>
2023-11-23 15:45:36 -0800

    flipped image issue

--- src/perception/image_shrink/scripts/shrink_image.py
+++ src/perception/image_shrink/scripts/shrink_image.py
@@ -15,7 +15,7 @@ class ShrinkImage:
 
         
     def callback(self, ros_rgb_image):
-        rgb_image = self.cv_bridge.imgmsg_to_cv2(ros_rgb_image, 'rgb8')
+        rgb_image = self.cv_bridge.imgmsg_to_cv2(ros_rgb_image, 'bgr8')
         rgb_image = cv2.resize(rgb_image, (0, 0), fx = self.scale, fy = self.scale) # 1/4th of the original size.
         imgmsg = self.cv_bridge.cv2_to_imgmsg(rgb_image)
         imgmsg.header.stamp = ros_rgb_image.header.stamp
@@ -26,7 +26,7 @@ class ShrinkImage:
         rospy.init_node('shrink_image', anonymous=False)
 
         self.scale = rospy.get_param('/image_shrink/downscale_ratio')
-        self.rgb_image_subscriber = rospy.Subscriber(rospy.get_param('/image_shrink/subscribe_topic'), Image,self.callback )
+        self.rgb_image_subscriber = rospy.Subscriber(rospy.get_param('/image_shrink/subscribe_topic'), Image, self.callback )
         self.pub = rospy.Publisher(rospy.get_param('/image_shrink/publish_topic'), Image, queue_size=1)
         
         self.cv_bridge = CvBridge()
