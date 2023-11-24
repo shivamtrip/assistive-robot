d8508a70ace595a0cb6f52fd02bc0cc5da9e3850
praveenVnktsh <pveenvenkatesh@gmail.com>
2023-11-24 11:09:00 -0500

    integrated DETIC

--- install_scripts/setup_brain.sh
+++ install_scripts/setup_brain.sh
@@ -11,6 +11,8 @@ sudo /usr/local/lib/obj_det_env/bin/pip3 install wheel ultralytics
 sudo /usr/local/lib/obj_det_env/bin/pip3 install -r ../src/perception/object_detection/config/requirements.txt
 sudo /usr/local/lib/obj_det_env/bin/pip3 install rospkg
 
+cd /usr/local/lib/obj_det_env/ && git clone git@github.com:facebookresearch/detectron2.git && cd detectron2 && sudo /usr/local/lib/obj_det_env/bin/python3 -m pip install -e .
+cd /usr/local/lib/obj_det_env/ && git clone https://github.com/facebookresearch/Detic.git --recurse-submodules && cd Detic && sudo /usr/local/lib/obj_det_env/bin/python3 -m pip install -r .
 echo "----------Setting up Grasp Detector----------"
 sudo python3 -m venv /usr/local/lib/graspnet_env
 sudo /usr/local/lib/graspnet_env/bin/pip3 install wheel
---
+++ src/perception/object_detection/scripts/detic_node.py
@@ -0,0 +1,173 @@
+import cv2
+
+
+import numpy as np
+import rospy
+from yolo.msg import DeticDetectionsRequest, DeticDetectionsResponse
+import message_filters
+from cv_bridge import CvBridge
+import time
+from sensor_msgs.msg import Image
+import torch
+import json
+import sys
+
+from detic.predictor import VisualizationDemo
+from detic.modeling.utils import reset_cls_test
+from detic.modeling.text.text_encoder import build_text_encoder
+from detic.config import add_detic_config
+from detectron2.engine import DefaultPredictor
+
+
+from detectron2.config import get_cfg
+from detectron2.data import MetadataCatalog, DatasetCatalog
+sys.path.insert(0, '/usr/local/lib/obj_det_env/Detic/third_party/CenterNet2/')
+from centernet.config import add_centernet_config
+
+
+class DeticNode:
+    
+    def __init__(self) -> None:
+        rospy.init_node('detic_node', anonymous=False)
+        
+        self.annotated_image_pub = rospy.Publisher(rospy.get_param('/object_detection/detic/annotation_topic'), Image, queue_size=1)
+        self.cv_bridge = CvBridge()
+        
+        self.upscale_fac = 1 # we will send full size images to detic.
+
+        self.setup_detectron()
+        self.warmup()
+        
+        self.server = rospy.Service('detic_predictions', DeticNode, self.callback)
+
+        rospy.loginfo(f"[{rospy.get_name()}] " + "Detic Ready...")
+
+    def setup_detectron(self):
+        BUILDIN_CLASSIFIER = {
+            'lvis': 'datasets/metadata/lvis_v1_clip_a+cname.npy',
+            'objects365': 'datasets/metadata/o365_clip_a+cnamefix.npy',
+            'openimages': 'datasets/metadata/oid_clip_a+cname.npy',
+            'coco': 'datasets/metadata/coco_clip_a+cname.npy',
+        }
+
+        BUILDIN_METADATA_PATH = {
+            'lvis': 'lvis_v1_val',
+            'objects365': 'objects365_v2_val',
+            'openimages': 'oid_val_expanded',
+            'coco': 'coco_2017_val',
+        }
+        
+        cfg = get_cfg()
+        add_centernet_config(cfg)
+        add_detic_config(cfg)
+        cfg.merge_from_file(rospy.get_param('/object_detection/detic/config_path'))
+        cfg.MODEL.WEIGHTS = rospy.get_param('/object_detection/detic/weight_path')
+        cfg.MODEL.ROI_HEADS.SCORE_THRESH_TEST = 0.5  # set threshold for this model
+        cfg.MODEL.ROI_BOX_HEAD.ZEROSHOT_WEIGHT_PATH = 'rand'
+        cfg.MODEL.ROI_HEADS.ONE_CLASS_PER_PROPOSAL = False # For better visualization purpose. Set to False for all classes.
+        cfg.MODEL.DEVICE= rospy.get_param('/object_detection/detic/device')
+        self.predictor = DefaultPredictor(cfg)
+        
+        if rospy.get_param('/object_detection/detic/custom_vocab'):
+            metadata = MetadataCatalog.get("__unused")
+            metadata.thing_classes = rospy.get_param('/object_detection/detic/class_list')
+            
+            classifier = self.get_clip_embeddings(metadata.thing_classes)
+        else:
+            vocabulary = rospy.get_param('/object_detection/detic/vocab_name')
+            metadata = MetadataCatalog.get(BUILDIN_METADATA_PATH[vocabulary])
+            classifier = BUILDIN_CLASSIFIER[vocabulary]
+        
+        num_classes = len(metadata.thing_classes)
+        reset_cls_test(self.predictor.model, classifier, num_classes)
+        
+        self.class_names = self.predictor.metadata.get("thing_classes", None)
+        
+
+    def warmup(self):
+        rospy.loginfo(f"[{rospy.get_name()}] " + "Warming up...")
+        img = np.zeros((640, 480, 3), dtype=np.uint8)
+        for i in range(10):
+            starttime = time.time()
+            self.run_model(img)
+            rospy.loginfo(f"Time taken: {time.time() - starttime}")
+        rospy.loginfo(f"[{rospy.get_name()}] " + "Warmed up!")
+
+    def get_clip_embeddings(self, vocabulary, prompt='a '):
+        text_encoder = build_text_encoder(pretrain=True)
+        text_encoder.eval()
+        texts = [prompt + x for x in vocabulary]
+        emb = text_encoder(texts).detach().permute(1, 0).contiguous().cpu()
+        return emb
+
+    def run_model(self, img):
+        predictions = self.predictor.predictor(img)
+        instances = predictions['instances'].to(torch.device("cpu"))
+        
+        pred_masks = list(instances.pred_masks)
+        scores = instances.scores.tolist()
+        class_indices = instances.pred_classes.tolist()
+        boxes = instances.pred_boxes.tensor.tolist()
+        
+        h, w = img.shape[:2]
+        
+        upscale_fac = self.upscale_fac
+        h *= upscale_fac
+        w *= upscale_fac
+        new_boxes = []
+        for b in boxes:
+            
+            x1, y1, x2, y2 = b
+            y_new1 = w - x2 * upscale_fac
+            x_new1 = y1 * upscale_fac
+            y_new2 = w - x1 * upscale_fac
+            x_new2 = y2 * upscale_fac
+            b = [x_new1, y_new1, x_new2, y_new2]
+            new_boxes.append(b)
+            
+        boxes = new_boxes.copy()
+        
+        seg_mask = np.zeros((img.shape[0], img.shape[1]), dtype=np.int32)
+
+        # largest to smallest order to reduce occlusion.
+        sorted_index = np.argsort([-mask.sum() for mask in pred_masks])
+        for i in sorted_index:
+            mask = pred_masks[i]
+            # label 0 is reserved for background label, so starting from 1
+            seg_mask[mask] = (i + 1)
+            
+        return seg_mask, boxes, class_indices, scores
+            
+    def callback(self, msg):
+        
+        ros_rgb_image = msg.image
+        rgb_image = self.cv_bridge.imgmsg_to_cv2(ros_rgb_image)
+        rotated_image = cv2.rotate(rgb_image, cv2.ROTATE_90_CLOCKWISE)
+        seg_mask, boxes, classes, confs = self.run_model(rotated_image)
+        
+        classes = classes.flatten()
+        boxes = boxes.flatten()
+        confs = confs.flatten().astype(np.float32)
+        nPredictions = len(classes)
+
+        seg_mask = cv2.resize(seg_mask, (0, 0), fx = 0.5, fy = 0.5)
+        self.annotated_image_pub.publish(self.cv_bridge.cv2_to_imgmsg(seg_mask))
+
+
+        response = DeticDetectionsResponse()
+        response.seg_mask = self.cv_bridge.cv2_to_imgmsg(seg_mask)
+        response.nPredictions  = nPredictions
+        response.box_bounding_boxes = boxes.tolist()
+        response.box_classes = classes.tolist()
+        response.confidences = confs.tolist()
+
+        return response
+
+    
+if __name__ == "__main__":
+    
+    node = DeticNode()
+    try:
+        rospy.spin()
+    except rospy.ROSInterruptException:
+        pass
